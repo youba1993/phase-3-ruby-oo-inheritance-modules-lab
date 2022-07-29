1c95d117c6ae5747778b2a1bee9b634919c9e6cb
youbaidjis <youba.etud@gmail.com>
2022-07-28 21:50:52 -0700

    Completed assignment

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,15 @@
 require 'pry'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
 
 class Artist
   attr_accessor :name
   attr_reader :songs
+
+  extend Memorable
+  extend Findable
+  include Paramable
 
   @@artists = []
 
@@ -11,20 +18,8 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -36,7 +31,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 require 'pry'
-
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable' 
 class Song
   attr_accessor :name
   attr_reader :artist
+
+  extend Memorable
+  extend Findable
+  include Paramable
 
   @@songs = []
 
@@ -10,27 +16,15 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
